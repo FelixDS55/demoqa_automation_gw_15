9358592af28de90c3e6153a765ccf42ed66bb44c
Михаил <turilovm@gmail.com>
2022-09-18 20:47:57 +0300

    added all files

---
+++ src/test/java/com/demoqa/TestDemoQa.java
@@ -0,0 +1,28 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestDemoQa {
+
+    @BeforeAll
+    static void configure(){
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void formTest(){
+        String firstName = "Mike";
+        String lastName = "Mercury";
+        String email = "mike@google.com";
+
+        open("/automation-practice-form");
+        $("#firstName").setValue("firstName");
+    }
+}
