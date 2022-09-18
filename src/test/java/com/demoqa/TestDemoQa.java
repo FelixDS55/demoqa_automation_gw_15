9db60bee8bb4517d3addbcff88dc97b91f4a8278
Михаил <turilovm@gmail.com>
2022-09-18 23:26:28 +0300

    added month and year

--- src/test/java/com/demoqa/TestDemoQa.java
+++ src/test/java/com/demoqa/TestDemoQa.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestDemoQa {
 
@@ -30,6 +29,19 @@ public class TestDemoQa {
         $("#userEmail").setValue(email);
         $("#gender-radio-1").doubleClick();
         $("#userNumber").setValue(mobile);
+        $("#dateOfBirthInput").click();
+        // выбор месяца
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__month-select").click();
+        //выбор года
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__year-select").click();
+        // выбор дня
+        $(".react-datepicker__day react-datepicker__day--003 react-datepicker__day--weekend").selectOption("3");
+
+
 
 
 
