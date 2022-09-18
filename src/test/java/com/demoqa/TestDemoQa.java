92b6bd5fa5012ed71243d54c402d340e846b1f4a
Михаил <turilovm@gmail.com>
2022-09-18 22:40:53 +0300

    added test

--- src/test/java/com/demoqa/TestDemoQa.java
+++ src/test/java/com/demoqa/TestDemoQa.java
@@ -3,6 +3,7 @@ package com.demoqa;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -21,8 +22,17 @@ public class TestDemoQa {
         String firstName = "Mike";
         String lastName = "Mercury";
         String email = "mike@google.com";
+        String mobile = "+79000000001";
 
         open("/automation-practice-form");
-        $("#firstName").setValue("firstName");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#gender-radio-1").doubleClick();
+        $("#userNumber").setValue(mobile);
+
+
+
+
     }
 }
