49a9e4329bfeb34d14a27f731ee09f2952b47d0d
FelixDS55 <81954013+FelixDS55@users.noreply.github.com>
2022-10-10 13:29:50 +0300

    Update CalendarComponents.java

--- src/test/java/com/demoqa/pages/components/CalendarComponents.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponents.java
@@ -5,8 +5,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class CalendarComponents {
 
     public CalendarComponents setDate(String day, String month, String year) {
-        $(".react-datepicker__month-select").selectOption("July");
-        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(yes );
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
 
         return this;
