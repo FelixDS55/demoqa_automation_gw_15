0c0e589a7c60de2e3cf857263e7c58b8f8e4e99b
FelixDS55 <81954013+FelixDS55@users.noreply.github.com>
2022-10-10 13:30:13 +0300

    Update CalendarComponents.java

--- src/test/java/com/demoqa/pages/components/CalendarComponents.java
+++ src/test/java/com/demoqa/pages/components/CalendarComponents.java
@@ -6,7 +6,7 @@ public class CalendarComponents {
 
     public CalendarComponents setDate(String day, String month, String year) {
         $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__year-select").selectOption(yes );
+        $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + day + ":not(.react-datepicker__day--outside-month)").click();
 
         return this;
